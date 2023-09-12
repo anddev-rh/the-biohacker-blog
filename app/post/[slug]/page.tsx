7f21e7dd481909d40299bbf41f0afe6594fe0736
Andres Rodriguez <andresrh222@gmail.com>
2023-09-11 20:19:00 -0500

    Support image in post

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import { Post } from "@/app/lib/interface";
 import { client } from "@/app/lib/sanity";
+import { urlFor } from "@/app/lib/sanityImageUrl";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 async function getData(slug: string) {
   const query = `*[_type == "post" && slug.current == "${slug}"][0]`
@@ -16,6 +18,14 @@ export default async function SlugPage({
   params: {slug: string}
 }) {
   const data = await getData(params.slug) as Post;
+
+  const PortableTextComponent = {
+    types: {
+      image: ({value}: {value: any}) => (
+        <Image src={urlFor(value).url()} alt="Image" className="rounded-lg" width={800} height={800}/>
+      )
+    }
+  }
 
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -41,7 +51,7 @@ export default async function SlugPage({
       <div className="divide-y divide-gray-200 pb-7 dark:divide-gray-700 xl:divide-y-0">
         <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
           <div className="prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
-            <PortableText value={data.content} components={{}} />
+            <PortableText value={data.content} components={PortableTextComponent} />
           </div>
         </div>
 
